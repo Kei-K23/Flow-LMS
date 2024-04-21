287b3de4bc1b8b64427d3f0f6222473200863c1d
Kei-K23 <keiksl2301@gmail.com>
2024-04-21 13:10:50 +0630

    add user progress table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 
 export const courses = pgTable("courses", {
@@ -8,3 +9,23 @@ export const courses = pgTable("courses", {
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at').defaultNow(),
 });
+
+export const courseRelations = relations(courses, ({ many }) => ({
+    userProgress: many(userProgress)
+}))
+
+export const userProgress = pgTable("user_progress", {
+    userId: text("user_id").primaryKey(),
+    username: text("username").notNull(),
+    userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+    activeCourseId: integer("active_course_id").references(() => courses.id, { onDelete: "cascade" }),
+    hearts: integer('hearts').notNull().default(5),
+    points: integer('points').notNull().default(0)
+})
+
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+    activeCourse: one(courses, {
+        fields: [userProgress.activeCourseId],
+        references: [courses.id]
+    })
+}))
